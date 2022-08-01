b1d88b1d8e8ed54b466bca745bf3809152c1ecac
codename-eth <110187783+codename-eth@users.noreply.github.com>
2022-08-01 15:48:55 -0700

    Add files via upload

--- objects/objects.js
+++ objects/objects.js
@@ -1,12 +1,12 @@
 var mapObjectsFull =
 [
-{name:'poi_dt_Police',id:'90001',type:'circle',location:'[-36.389706, -19.944796]',pimage:'poi_dt_Police.png',pcaption:'Police Station',description:'The Police station ... '},
-{name:'poi_dt_Pub',id:'90002',type:'circle',location:'[-32.250805, -7.985151]',pimage:'poi_dt_Pub.png',pcaption:'Downtown Pub',description:'The Downtown Pub ...'},
-{name:'poi_dt_UrbanHabitat',id:'90003',type:'circle',location:'[-39.1016, 6.876466]',pimage:'poi_dt_UrbanHabitat.png',pcaption:'Urban Habitat',description:'The residence building Urban Habitat ...'},
-{name:'poi_dt_Diamond',id:'90004',type:'polygon',location:'[[-30.675378, 10.569885],[-29.992152, 17.868786],[-31.952773, 18.22054],[-32.917804, 10.833701]]',pimage:'poi_dt_Diamond.png',pcaption:'Diamond District',description:'The Diamond District downtown provides ... '},
-{name:'mem_dt_ArtExhibit',id:'90005',type:'villain',location:'[-36.666372, -20.347289]',pimage:'mem_dt_ArtExhibit.png',pcaption:'Villain Art Exhibition',description:'The villain known as ...'},
-{name:'mem_dt_Lemonade',id:'90006',type:'hero',location:'[-33.519947, -43.945313]',pimage:'mem_dt_Lemonade.png',pcaption:'Lemonade Orders',description:'The hero known as the duck ordered lemonade.'},
-{name:'poi_dt_ConventionCenter',id:'90007',type:'polygon',location:'[[-24.026587, -5.359563],[-23.704808, 4.662652],[-30.435271, 5.717622],[-31.79084, -3.952936]]',pimage:'default.png',pcaption:'Convention Center',description:'The convention center in downtown genesis city provides luxary accomodations for conferences and symposia across the frontier.'},
-{name:'poi_dt_DumaTower',id:'90008',type:'villain',location:'[-40.583439, 19.518474]',pimage:'default.png',pcaption:'Duma Industries Tower',description:'Duma industries central base of operations.  A tall tower with ocean views.'}
+{name:'poi_dt_Police',id:'90001',type:'circle',location:[-36.389706, -19.944796],pimage:'poi_dt_Police.png',pcaption:'Police Station',description:'The Police station ... '},
+{name:'poi_dt_Pub',id:'90002',type:'circle',location:[-32.250805, -7.985151],pimage:'poi_dt_Pub.png',pcaption:'Downtown Pub',description:'The Downtown Pub ...'},
+{name:'poi_dt_UrbanHabitat',id:'90003',type:'circle',location:[-39.1016, 6.876466],pimage:'poi_dt_UrbanHabitat.png',pcaption:'Urban Habitat',description:'The residence building Urban Habitat ...'},
+{name:'poi_dt_Diamond',id:'90004',type:'polygon',location:[[-30.675378, 10.569885],[-29.992152, 17.868786],[-31.952773, 18.22054],[-32.917804, 10.833701]],pimage:'poi_dt_Diamond.png',pcaption:'Diamond District',description:'The Diamond District downtown provides ... '},
+{name:'mem_dt_ArtExhibit',id:'90005',type:'villain',location:[-36.666372, -20.347289],pimage:'mem_dt_ArtExhibit.png',pcaption:'Villain Art Exhibition',description:'The villain known as ...'},
+{name:'mem_dt_Lemonade',id:'90006',type:'hero',location:[-33.519947, -43.945313],pimage:'mem_dt_Lemonade.png',pcaption:'Lemonade Orders',description:'The hero known as the duck ordered lemonade.'},
+{name:'poi_dt_ConventionCenter',id:'90007',type:'polygon',location:[[-24.026587, -5.359563],[-23.704808, 4.662652],[-30.435271, 5.717622],[-31.79084, -3.952936]],pimage:'default.png',pcaption:'Convention Center',description:'The convention center in downtown genesis city provides luxary accomodations for conferences and symposia across the frontier.'},
+{name:'poi_dt_DumaTower',id:'90008',type:'circle',location:[-40.583439, 19.518474],pimage:'default.png',pcaption:'Duma Industries Tower',description:'Duma industries central base of operations.  A tall tower with ocean views.'}
 ];     
                 
